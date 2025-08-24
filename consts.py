81f5a17260f40aa3b5ea515100f030d9e328de28
jbt <amitleviner1@gmail.com>
2025-08-24 14:39:35 +0300

    consts

---
+++ consts.py
@@ -0,0 +1,34 @@
+#SCREEN SIZE
+ROWS = 25
+COLS = 50
+
+
+CELL_SIZE = 20
+
+# WINDOOW SIZE
+WINDOW_WIDTH = COLS * CELL_SIZE
+WINDOW_HEIGHT = ROWS * CELL_SIZE
+
+# COLORS
+BACKGROUND_COLOR = (3, 252, 136)
+
+
+GREEN = (34, 139, 34)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# PICTURES
+SOLDIER_IMG = "bin/soldier.png"
+SOLDIER_NIGTH_IMG = "bin/soldier_nigth.png"
+DINO_IMG = "bin/soldier(2).png"
+SNAKE_IMG = "bin/snake.png"
+INJURY_IMG = "bin/injury.png"
+
+FLAG_IMG = "bin/flag.png"
+MINE_IMG = "bin/mine.png"
+GRASS_IMG = "bin/grass.png"
+EXPLOSION_IMG = "bin/explotion.png"
+
+# NUMBERS
+NUM_MINES = 20
+NUM_BUSHS = 20
